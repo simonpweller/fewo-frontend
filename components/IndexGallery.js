363f54acb12e456420783b4d1658c11748ba800e
Simon Weller <s.meierbeck@gmail.com>
2019-04-05 17:43:26 +0200

    Adding srcSet for images

--- components/IndexGallery.js
+++ components/IndexGallery.js
@@ -2,22 +2,30 @@ import React from 'react';
 import ImageGallery from 'react-image-gallery';
 
 const IndexGallery = () => {
+
+  const image1 = require('../images/1.jpg?resize&sizes[]=480&sizes[]=960');
+  const image2 = require('../images/2.jpg?resize&sizes[]=480&sizes[]=960');
+  const image3 = require('../images/3.jpg?resize&sizes[]=480&sizes[]=960');
+
   const images = [
     {
-      original: require('../images/1.jpg?resize&size=960'),
-      thumbnail: require('../images/1.jpg?resize&size=100'),
+      original: image1.src,
+      srcSet: image1.srcSet,
+      sizes: "(max-width: 960px) 100vw, 960px"
     },
     {
-      original: require('../images/2.jpg?resize&size=960'),
-      thumbnail: require('../images/2.jpg?resize&size=100'),
+      original: image2.src,
+      srcSet: image2.srcSet,
+      sizes: "(max-width: 960px) 100vw, 960px"
     },
     {
-      original: require('../images/3.jpg?resize&size=960'),
-      thumbnail: require('../images/3.jpg?resize&size=100'),
-    }
+      original: image3.src,
+      srcSet: image3.srcSet,
+      sizes: "(max-width: 960px) 100vw, 960px"
+    },
   ];
 
-  return <ImageGallery items={images} />;
+  return <ImageGallery items={images} showThumbnails={false} />;
 };
 
 export default IndexGallery;
