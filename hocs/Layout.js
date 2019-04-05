3ba68499a70a523e38b94b8101ed2276b5209e47
Simon Weller <s.meierbeck@gmail.com>
2019-04-05 18:38:16 +0200

    Cleaning up layout a bit

--- hocs/Layout.js
+++ hocs/Layout.js
@@ -8,23 +8,16 @@ import '../styles/styles.scss'
 export default function Layout({ locale, setLocale, children }) {
 
   return (
-    <>
+    <div className="layout">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <style jsx global>
-        {`
-          .leaflet-container {
-            height: 500px;
-          }
-        `}
-      </style>
 
       <Navbar locale={locale} setLocale={setLocale} />
       <main className="container">
         {children}
       </main>
       <Footer/>
-    </>
+    </div>
   )
 }
