83c0331f928325c6479c270fab87e6ac5e468f98
Simon Weller <s.meierbeck@gmail.com>
2019-04-06 12:09:15 +0200

    Started building out tiles

--- components/Accommodations/Accommodations.js
+++ components/Accommodations/Accommodations.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, injectIntl} from 'react-intl';
 import css from './accommodations.scss';
 
-const Accommodations = () => {
+const Accommodations = ({intl}) => {
+
+  const imgApartment = require('../../images/FW.jpg?resize&sizes[]=200&sizes[]=400');
+  const imgHouse = require('../../images/FH.jpg?resize&sizes[]=200&sizes[]=400');
+  const sizes = '(max-width: 456px) 100vw, (max-width: 1200px) 50vw, 600px';
+
   return (
     <div className={css.accommodations}>
-      <div className="tile">
-
-        <h3>
+      <div className={css.tile}>
+        <img
+          src={imgApartment.src}
+          srcSet={imgApartment.srcSet}
+          sizes={sizes}
+          alt={intl.formatMessage({id: 'apartment'})}
+        />
+        <h3 className={css.title}>
           <FormattedMessage id={'apartment'}/>
         </h3>
       </div>
-      <div className="tile">
-        <h3>
+      <div className={css.tile}>
+        <img
+          src={imgHouse.src}
+          srcSet={imgHouse.srcSet}
+          sizes={sizes}
+          alt={intl.formatMessage({id: 'house'})}
+        />
+        <h3 className={css.title}>
           <FormattedMessage id={'house'}/>
         </h3>
       </div>
@@ -20,4 +36,4 @@ const Accommodations = () => {
   );
 };
 
-export default Accommodations;
+export default injectIntl(Accommodations);
