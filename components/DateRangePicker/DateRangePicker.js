583712205d7d1c4bc39b8c8a56b2862985720594
Simon Weller <s.meierbeck@gmail.com>
2019-06-16 12:15:16 +0200

    Localized DateRangePicker

--- components/DateRangePicker/DateRangePicker.js
+++ components/DateRangePicker/DateRangePicker.js
@@ -2,9 +2,9 @@ import React, {useState} from 'react';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import './DateRangePicker.css';
+import {injectIntl} from 'react-intl';
 
-const DateRangePicker = () => {
-
+const DateRangePicker = ({intl}) => {
   const [from, setFrom] = useState(undefined);
   const [to, setTo] = useState(undefined);
 
@@ -14,8 +14,48 @@ const DateRangePicker = () => {
     setTo(range.to);
   }
 
+  const WEEKDAYS_LONG = [
+    intl.formatMessage({id: 'Sunday'}),
+    intl.formatMessage({id: 'Monday'}),
+    intl.formatMessage({id: 'Tuesday'}),
+    intl.formatMessage({id: 'Wednesday'}),
+    intl.formatMessage({id: 'Thursday'}),
+    intl.formatMessage({id: 'Friday'}),
+    intl.formatMessage({id: 'Saturday'}),
+  ];
+
+  const WEEKDAYS_SHORT = [
+    intl.formatMessage({id: 'SundayShort'}),
+    intl.formatMessage({id: 'MondayShort'}),
+    intl.formatMessage({id: 'TuesdayShort'}),
+    intl.formatMessage({id: 'WednesdayShort'}),
+    intl.formatMessage({id: 'ThursdayShort'}),
+    intl.formatMessage({id: 'FridayShort'}),
+    intl.formatMessage({id: 'SaturdayShort'}),
+  ];
+
+  const MONTHS = [
+    intl.formatMessage({id: 'January'}),
+    intl.formatMessage({id: 'February'}),
+    intl.formatMessage({id: 'March'}),
+    intl.formatMessage({id: 'April'}),
+    intl.formatMessage({id: 'May'}),
+    intl.formatMessage({id: 'June'}),
+    intl.formatMessage({id: 'July'}),
+    intl.formatMessage({id: 'August'}),
+    intl.formatMessage({id: 'September'}),
+    intl.formatMessage({id: 'October'}),
+    intl.formatMessage({id: 'November'}),
+    intl.formatMessage({id: 'December'}),
+  ];
+
   return (
     <DayPicker
+      locale={intl.locale}
+      firstDayOfWeek={parseFloat(intl.formatMessage({id: 'firstDayOfWeek'}))}
+      weekdaysLong={WEEKDAYS_LONG}
+      weekdaysShort={WEEKDAYS_SHORT}
+      months={MONTHS}
       className="Selectable"
       numberOfMonths={2}
       selectedDays={[from, { from, to }]}
@@ -25,4 +65,4 @@ const DateRangePicker = () => {
   );
 };
 
-export default DateRangePicker;
+export default injectIntl(DateRangePicker);
